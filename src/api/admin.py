374d3c2371a4e4478677d696a1b995be9b6fb8c4
alonsogomezo <luisalonsogo95@gmail.com>
2022-06-24 01:56:58 +0000

    hola

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Characters, Planets, Characters_Favorites, Favorite_Planets
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,10 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Characters, db.session))
+    admin.add_view(ModelView(Planets, db.session))
+    admin.add_view(ModelView(Characters_Favorites, db.session))
+    admin.add_view(ModelView(Favorite_Planets, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,67 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String(200), unique=True, nullable=False)
+    gender = db.Column(db.String(100))
+    url = db.Column(db.String(500), unique=True)
+
+    def __repr__(self):
+        return '<Characters %r>' % self.character_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "character_name": self.character_name,
+            "gender": self.gender,
+            "url": self.url,
+            # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_name = db.Column(db.String(200), unique=True, nullable=False)
+    orbit = db.Column(db.String(200))
+    url = db.Column(db.String(500), unique=True)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.planet_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet_name": self.planet_name,
+            "orbit": self.orbit,
+            "url": self.url,
+            # do not serialize the password, its a security breach
+        }
+
+class Characters_Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String(200), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Characters_Favorites %r>' % self.character_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "character_name": self.character_name,
+            # do not serialize the password, its a security breach
+        }
+
+class Favorite_Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_name = db.Column(db.String(200), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Favorite_Planets %r>' % self.planet_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet_name": self.planet_name,
+            # do not serialize the password, its a security breach
+        }
